0e3fca5d8e7467369888ed78bf70441378554c4c
vladyslav-lutchyn <vladyslav.lutchyn@vitechteam.com>
2024-08-11 22:46:54 -0400

    Added page header

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,9 +1,21 @@
+import {Input} from "@/components/ui/input.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {ListFilter} from "lucide-react";
+import PageHeaderSection from "@/components/Dashboard/DashboardHeaderSection/PageHeaderSection.tsx";
 
 
 export default function CardListPage() {
     return (
-        <div>
-            card list page
+        <div className="layout-wrapper">
+            <PageHeaderSection
+                LeftCornerSection={() => (
+                    <>
+                        <div className="groups-header-section">
+                            <Input placeholder="Search" className="groups-header-input"/>
+                            <Button variant="outline" className="groups-header-button"><ListFilter /></Button>
+                        </div>
+                    </>
+                )}/>
         </div>
     );
 };
