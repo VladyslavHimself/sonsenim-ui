4f921c64c7d98e94394ff70232676850b73b181b
vladyslavhimself <vladyslavhimself@gmail.com>
2024-11-08 20:27:09 -0500

    Added profile popover

--- src/components/AvatarProfile/AvatarProfile.tsx
+++ src/components/AvatarProfile/AvatarProfile.tsx
@@ -1,12 +1,16 @@
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
 import AvatarLogo from "@/assets/Icons/avatar.png";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover.tsx";
-import {ChevronDown} from "lucide-react";
+import {ChevronDown, LifeBuoyIcon, MailsIcon} from "lucide-react";
 import useUser from "@/api/user/useUser.ts";
 import './AvatarProfile.scss';
+import {Button} from "@/components/ui/button.tsx";
+import {AvatarIcon} from "@radix-ui/react-icons";
+import {useNavigate} from "react-router-dom";
 
 export default function AvatarProfile() {
     const { userData } = useUser();
+    const navigate = useNavigate();
 
     return (
         <div className="avatar-profile">
@@ -17,8 +21,12 @@ export default function AvatarProfile() {
             <span>{userData?.username}</span>
             <Popover>
                 <PopoverTrigger><ChevronDown color="orange"/></PopoverTrigger>
-                <PopoverContent>Lorem ipsum dolor sit amet.</PopoverContent>
+                <PopoverContent sideOffset="2" align="end" className="profile-popover">
+                    <Button variant="ghost" onClick={() => navigate('/profile')}><AvatarIcon style={{ width: 32, height: 32}} />Account</Button>
+                    <Button variant="ghost"><MailsIcon style={{ width: 30, height: 30}} />Notifications</Button>
+                    <Button variant="ghost"><LifeBuoyIcon style={{ width: 30, height: 30}} />Support</Button>
+                </PopoverContent>
             </Popover>
         </div>
     );
-};
+}
