b127cecbc150ede5430d81e730c74fa1b230e2aa
vladyslavhimself <vladyslavhimself@gmail.com>
2024-11-08 22:43:57 -0500

    Added possibility to search groups in quicksearch

--- src/pages/GroupsList.tsx
+++ src/pages/GroupsList.tsx
@@ -20,9 +20,9 @@ export default function GroupsList() {
     const [searchInput, setSearchInput] = useState('');
 
     const filteredGroups = useMemo(() => {
-        if (searchInput) {
-
-            return [];
+        if (searchInput && groupsInfo) {
+            return groupsInfo.filter((group) =>
+                group.groupName.toLowerCase().includes(searchInput.toLowerCase()));
         }
 
         return groupsInfo;
