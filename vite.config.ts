c7449c0aa5082e968351d4c8e9596385d6daa585
vladyslavhimself <vladyslavhimself@gmail.com>
2024-11-24 15:10:56 -0500

    Initialized PWA application

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,44 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import {nodePolyfills} from "vite-plugin-node-polyfills";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Sonsenim LMS',
+        short_name: 'Sonsenim',
+        theme_color: '#FAFAFA',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
