dc7f5a86555a2ce291430c0e6d726917303e1a13
vladyslavhimself <vladyslavhimself@gmail.com>
2024-12-08 21:07:15 -0500

    Minor fixes. * Changed PageContentHeaderSection in `SelectedGroup` page, while in mobile

--- src/pages/SelectedGroupPage.tsx
+++ src/pages/SelectedGroupPage.tsx
@@ -12,8 +12,11 @@ import CreateNewDeckModal from "@/components/Modals/DeckModals/CreateNewDeckModa
 import { useState } from "react";
 import {DeckWithAggregatedDataResponse} from "@/api/decks/decks.ts";
 import useQuicksearch from "@/hooks/useQuicksearch.ts";
-
+import PageHeaderSectionTitle
+    from "@/components/Dashboard/DashboardHeaderSection/PageHeaderSectionTitle/PageHeaderSectionTitle.tsx";
+import {useMediaQuery} from "react-responsive";
 export default function SelectedGroupPage() {
+    const isMobile = useMediaQuery({query: "(max-width: 700px)"});
     const navigate = useNavigate();
     const location = useLocation();
     const { groupId } = useParams();
@@ -21,30 +24,30 @@ export default function SelectedGroupPage() {
     const [searchInput, setSearchInput] = useState('');
     const filteredDecks = useQuicksearch<DeckWithAggregatedDataResponse>(aggregatedDecks!, ['deckName'], searchInput);
 
+
+    // TODO: Make context for header || HARDCODED
+    const ContentSectionHeader = isMobile ? MobileSearchbar
+        : SelectedGroupDesktopContentHeader;
+
     return (
         <div className="layout-wrapper">
             <PageHeaderSection>
-                <div className="groups-header-section">
+                {!isMobile && <div className="groups-header-section">
                     <Input
-                           placeholder="Search"
-                           className="groups-header-input"
-                           value={searchInput}
-                           onChange={(e) => {
-                               setSearchInput(e.target?.value)}}
+                        placeholder="Search"
+                        className="groups-header-input"
+                        value={searchInput}
+                        onChange={(e) => {
+                            setSearchInput(e.target?.value)
+                        }}
                     />
                     {
                         // #TODO: Insert sorting combobox
                     }
-                </div>
+                </div> || <PageHeaderSectionTitle>{location.state?.groupName}</PageHeaderSectionTitle>}
             </PageHeaderSection>
 
-            <CardsListContentSection Header={() => (
-                <>
-                    <h1>{location.state?.groupName}</h1>
-                    <Button style={{padding: "25px 30px"}} onClick={() => onCreateNewDeckModal(+groupId!)}>+ Create a
-                        new deck</Button>
-                </>
-            )}>
+            <CardsListContentSection searchInput={searchInput} setSearchInput={setSearchInput} Header={ContentSectionHeader}>
                 {
                     // TODO: Add template (design), if user haven't any decks here
                     filteredDecks?.map((deck) => (
@@ -70,6 +73,34 @@ export default function SelectedGroupPage() {
         </div>
     );
 
+
+}
+
+
+// TODO: DUPLICATED IN `GroupsList.tsx`. Move in separate component
+function MobileSearchbar({ searchInput, setSearchInput}: any) {
+    return <Input
+        placeholder="Search"
+        className="groups-header-input"
+        value={searchInput}
+        onChange={(e) => {
+            setSearchInput(e.target?.value)
+        }}
+    />
+}
+
+// TODO: ALSO MOVE TO SEPARATE COMPONENT
+function SelectedGroupDesktopContentHeader() {
+    const location = useLocation();
+    const { groupId } = useParams();
+
+    return <>
+        <h1>{location.state?.groupName}</h1>
+        <Button style={{padding: "25px 30px"}} onClick={() => onCreateNewDeckModal(+groupId!)}>+ Create a
+            new deck</Button>
+    </>
+
+    // TODO: TO AVOID REDUNDANT REFETECH - MOVE THEM TO `onClose` CALLBACK
     function onCreateNewDeckModal(groupId: number) {
         return ModalBoxes.open({
             className: 'create-new-deck-modal',
@@ -78,4 +109,4 @@ export default function SelectedGroupPage() {
             onClose: () => {}
         })
     }
-};
+}
