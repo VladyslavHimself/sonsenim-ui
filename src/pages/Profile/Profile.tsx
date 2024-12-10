71d1e8ec859148375410e3aa510fea4113ab6fdb
vladyslavhimself <vladyslavhimself@gmail.com>
2024-12-10 13:38:52 -0500

    Adapt Profile page for mobile responsiveness
    Added responsive design to the Profile page with media queries to improve layout on smaller screens. Updated styling for buttons and content cards to ensure consistency and better usability on mobile devices.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,23 +5,36 @@ import {Button} from "@/components/ui/button.tsx";
 import ContentCard from "@/components/ContentCard/ContentCard.tsx";
 import useUser from "@/api/user/useUser.ts";
 import dayjs from "dayjs";
+import {useMediaQuery} from "react-responsive";
+import PageHeaderSectionTitle
+    from "@/components/Dashboard/DashboardHeaderSection/PageHeaderSectionTitle/PageHeaderSectionTitle.tsx";
 
 export default function Profile() {
+    const isMobile = useMediaQuery({query: "(max-width: 700px)"});
     const { userData } = useUser();
     const { firstName, lastName, email, totalDecks, totalCards, createdAt } = userData || {};
 
     return (
         <div className="layout-wrapper profile-page">
             <PageHeaderSection>
-                <div className="profile-page-user-header">
+                {!isMobile ? <div className="profile-page-user-header">
+                    <div className="user-info-avatar"><CircleUserRound type="filled"/></div>
+                    <div className="user-info-content">
+                        <div className="user-info-content-fullname">{`${firstName} ${lastName}`}</div>
+                        <div className="user-info-content-email">{email || '-'}</div>
+                    </div>
+                </div> : <PageHeaderSectionTitle>Profile</PageHeaderSectionTitle>}
+            </PageHeaderSection>
+
+            {
+                isMobile && <div className="profile-page-user-header">
                     <div className="user-info-avatar"><CircleUserRound type="filled"/></div>
                     <div className="user-info-content">
                         <div className="user-info-content-fullname">{`${firstName} ${lastName}`}</div>
                         <div className="user-info-content-email">{email || '-'}</div>
                     </div>
                 </div>
-            </PageHeaderSection>
-
+            }
             <div className="profile-page-group-section">
                 <div className="profile-page-group-section-header">Subscription details</div>
                 <div className="profile-page-group-section-content">
@@ -29,13 +42,13 @@ export default function Profile() {
                         <div className="profile-page-group-section-item">Current tier - <span>free</span></div>
                         <div className="profile-page-group-section-item">During - <span>lifetime</span></div>
                     </div>
-                    <Button>Buy premium</Button>
+                    <Button className="profile-page-group-premium-button">Buy premium</Button>
                 </div>
             </div>
             <div className="profile-page-content-strip">
-                <ContentCard title="Member since" information={dayjs(createdAt).format("YYYY/MM/DD")} />
-                <ContentCard title="Total decks" information={totalDecks?.toString()} />
-                <ContentCard title="Total cards" information={totalCards?.toString()} />
+                <ContentCard title="Member since" information={dayjs(createdAt).format("YYYY/MM/DD")}/>
+                <ContentCard title="Total decks" information={totalDecks?.toString()}/>
+                <ContentCard title="Total cards" information={totalCards?.toString()}/>
             </div>
         </div>
     );
